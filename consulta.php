1724363e629c8b9c944c0f94d31ebd1bcb0c9b3b
Marcella <marszpless@gmail.com>
2024-08-08 16:46:40 -0300

    Aula 2

---
+++ consulta.php
@@ -0,0 +1,14 @@
+<?
+include "conecta.php";
+
+echo "<br><b>Mostrando todos os registros</b><br><br>";
+$sql = "SELECT id, descrição, fabricante, quantidade, preco_custo, preco_venda, imagem FROM produtos";
+$resultado = mysqli_query($conexao, $sql);
+if (mysqli_num_rows($resultado) > 0) {
+    while($registro = mysqli_fetch_assoc($resultado)) {
+        echo "Id: " . $registro["id"]. "Descrição: " . $registro["descrição"]. "Fabricante: " . $registro["fabricante"]. "Preço custo " . $registro["preco_custo"]. "Preço venda: ".$registro["preco_venda"]."Imagem".$registro["imagem"]."<br>";
+    }
+} else {
+    echo "Nenhum registro encontrado.";
+}
+?>
